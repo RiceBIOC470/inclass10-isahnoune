fda51d9bc9ad0e7c74ee0db3234677eaa0ffe250
isahnoune <31284645+isahnoune@users.noreply.github.com>
2017-10-06 18:32:18 -0500

    Add files via upload

--- inclass10.m
+++ inclass10.m
@@ -5,14 +5,35 @@
 %1. Use matlab to perform pca on this data, getting the transformation
 %matrix, the transformed data, and the eigenvalues.
 
+load('example_dat.mat');
+[coeff, sc, eig] = pca(dat);
+
 %2. Look at the eigenvalues - how many dimensions are necessary to explain
 %the data? 
+
+% According to the output, there were 8 eigenvalues generated, but the data
+% can be explained in 2 dimensions given that the remaining values are very low.
 
 %3. Using kmeans clustering to cluster the data into 3 clusters. 
 % Make a plot of the first coordinate of the raw data vs the second color coded
 % by which cluster it is in. Make the same plot but use the first two
 % principle components instead. 
 
-%4. Repeat part 3 but use 7 clusters. Which is a more appropriate number of
-%clusters for the data?
+ids = kmeans(dat,3);
+figure;
+scatter(dat(:,1),dat(:,2),[],ids(:,1));
 
+scatter(sc(:,1),sc(:,2),[],ids(:,1));
+
+%4. Repeat part 3 but use 7 clusters. Which is a more appropriate number of
+%clusters for the data? 
+
+ids2 = kmeans(dat,7);
+figure;
+scatter(dat(:,1),dat(:,2),[],ids(:,1));
+
+figure;
+scatter(sc(:,1),sc(:,2),[],ids(:,1));
+
+%Given the generated figures, 3 clusters are more appropriate as the
+%clusters are clearly delineated. 
